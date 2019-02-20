51af48556a6c1ffbaf7b38512505686736afb6a8
McKell Stauffer <mckell.stauffer@gmail.com>
2019-02-20 12:50:22 -0700

    adding VLAE into Rainbow

---
+++ Rainbow/vlae/dataset/__init__.py
@@ -0,0 +1,6 @@
+try:    # Works for python 3
+    from vlae.dataset.dataset import *
+    from vlae.dataset.dataset_atari import AtariDataset
+except: # Works for python 2
+    from vlae.dataset import *
+    from vlae.dataset.dataset_atari import AtariDataset
